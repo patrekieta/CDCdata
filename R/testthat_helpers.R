f2bc78f80ea31d827552c9ed5eaf568a88eda47d
patrekieta <Patrick.Rekieta@gmail.com>
2026-03-20 17:45:15 -0500

    update docs for metadata

--- R/testthat_helpers.R
+++ R/testthat_helpers.R
@@ -1,18 +1,1 @@
 # Test helper functions
-
-# Check if we have internet connectivity
-skip_if_offline <- function() {
-  if (!curl::has_internet()) {
-    skip("No internet connection")
-  }
-
-  # Also check if CDC API is reachable
-  tryCatch({
-    httr2::request("https://data.cdc.gov") |>
-      httr2::req_timeout(5) |>
-      httr2::req_perform()
-  }, error = function(e) {
-    skip("CDC API not reachable")
-  })
-}
-
